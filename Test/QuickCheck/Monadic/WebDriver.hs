cb046f79958ca21241e4c75bdefb27b52d9435df
Christian Brink <christianpbrink@gmail.com>
2014-06-25 17:28:33 +0200

    Added documentation

--- Test/QuickCheck/Monadic/WebDriver.hs
+++ Test/QuickCheck/Monadic/WebDriver.hs
@@ -5,7 +5,11 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings   #-}
 
-module Test.QuickCheck.Monadic.WebDriver where
+module Test.QuickCheck.Monadic.WebDriver 
+    (
+    -- * Example
+    -- $example
+    ) where
 
 import Prelude
 
@@ -18,9 +22,19 @@ import Test.WebDriver
 import Data.Monoid                            ((<>))
 import Control.Monad.IO.Class
 
+-- |Patterned after `Test.QuickCheck.Monadic.monadicIO`.
 monadicWD capabilities wd = monadic wdProperty wd
     where runSesh    = runSession defaultSession capabilities
           wdProperty = ioProperty . runSesh
 
 runIO :: IO a -> PropertyM WD a
 runIO = QCM.run . liftIO
+
+-- $example
+-- > import Test.QuickCheck
+-- > import Test.QuickCheck.Monadic (run, pick, assert)
+-- > import Test.WebDriver
+-- > prop_Example = monadicWD allCaps $ do
+-- >     pick $ oneof ("http://www.yesodweb.com", "http://snapframework.com/")
+-- >     run $ openPage url
+-- >     assert True
