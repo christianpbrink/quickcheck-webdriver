5d87e8e400f6c072ab35783aa29329783a9fc1fa
Christian Brink <christianpbrink@gmail.com>
2014-06-25 21:53:00 +0200

    Added helper fns

--- Test/QuickCheck/Monadic/WebDriver.hs
+++ Test/QuickCheck/Monadic/WebDriver.hs
@@ -13,38 +13,57 @@
 -- License:     MIT
 -- Maintainer:  Christian Brink
 -- 
--- * Example
--- $example
-
-module Test.QuickCheck.Monadic.WebDriver where
-
-import Prelude
-
-import Test.QuickCheck
-import Test.QuickCheck.Property
-import Test.QuickCheck.Monadic as QCM
-
-import Test.WebDriver
-
-import Data.Monoid                            ((<>))
-import Control.Monad.IO.Class
-
--- |Patterned after `Test.QuickCheck.Monadic.monadicIO`.
-monadicWD capabilities wd = monadic wdProperty wd
-    where runSesh    = runSession defaultSession capabilities
-          wdProperty = ioProperty . runSesh
-
-runIO :: IO a -> PropertyM WD a
-runIO = QCM.run . liftIO
-
--- $example
+-- Example usage:
+--
 -- > import Test.QuickCheck
 -- > import Test.QuickCheck.Monadic (run, pick, assert)
 -- > import Test.WebDriver
--- > prop_Example = monadicWD allCaps $ do
+-- > prop_Example = monadicWD context $ do
 -- >     url <- pick $ oneof ("http://www.yesodweb.com", "http://snapframework.com/")
 -- >     run $ openPage url
 -- >     assert True
 -- >     runIO $ putStrLn $ "We've just evaluated a `Gen a` using `pick` and \
 -- >                        \a `WD ()` using `run`, then asserted a that a \
 -- >                        \property holds using `assert`."
+-- >  where context = SessionParams allCaps (return ())
+
+module Test.QuickCheck.Monadic.WebDriver where
+
+import Prelude
+import Test.QuickCheck
+import Test.QuickCheck.Property
+import Test.QuickCheck.Monadic as QCM
+import Test.WebDriver
+import Control.Monad.IO.Class
+
+data Context = ExistingSession WDSession 
+             | SessionParams Capabilities (WD ())
+
+-- |Patterned after `monadicIO` (and `ioProperty`).
+monadicWD context wd = monadic wdProperty wd
+  where 
+    wdProperty = ioProperty . runSesh
+    runSesh action = case context of
+        ExistingSession sesh -> runWD sesh action
+        SessionParams caps setup -> runSession defaultSession caps $ setup >> action
+          
+
+runIO :: IO a -> PropertyM WD a
+runIO = QCM.run . liftIO
+
+chromeOpts = [ "--log-level=0" ]
+
+chrome' = chrome { chromeOptions = chromeOpts }
+
+caps browser = allCaps {browser=browser} 
+
+capsChrome = caps chrome'
+capsFirefox = caps firefox
+
+seshParams browser = SessionParams (caps browser) $ return ()
+
+seshParamsChrome   = seshParams chrome'
+seshParamsFirefox' = seshParams firefox
+
+-- $example
+--
